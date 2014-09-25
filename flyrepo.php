d47496b25cc2e365cad3a663836b1baae369c0d0
Jonas Felix <jonas.felix@typo3.org>
2014-09-25 16:52:59 +0200

    cli php script

--- flyrepo.php
+++ flyrepo.php
@@ -1,3 +1,12 @@
 <?php
 
-echo "Hello, I am flyrepo and I'll make distributed open source development even more fun.\n";
+echo "FlyRepo: Live is to short to build packages!\n\n";
+
+// temporary, will be replaced by autoloader
+require_once('src/FlyRepo.php');
+
+try {
+	$flyRepo = \cabservicesag\FlyRepo\FlyRepo::open('./');
+} catch (Exception $flyRepoException ) {
+	print($flyRepoException->getMessage()."\n");	
+}
