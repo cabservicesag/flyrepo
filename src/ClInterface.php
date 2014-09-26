2e7b4ed0ca2672825ad10ab262450a1d69860181
Jonas Felix <jonas.felix@icloud.com>
2014-09-26 10:22:07 +0200

    add module based command framework and option parsing

--- src/ClInterface.php
+++ src/ClInterface.php
@@ -21,9 +21,19 @@ namespace cabservicesag\FlyRepo;
 
 class ClInterface {
 	public $flyRepo = false;
-	private $argv = array();
-	private $commandName = false;
+	
+	public $argv = array();
+	
 	private $command = false;
+	private $commandName = false;
+	
+	public $usageInfo = "FlyRepo: *Cause live is to short to build packages*\n\nUsage: flyrepo.php <command> \n\n";
+	public $availableOptions = array(
+		'C:' => 'Change working directory.'
+		);
+	public $dir = false;
+	public $options = array();
+	
 	
 	/**
 	 * ClInterface::run($argv)
@@ -40,13 +50,7 @@ class ClInterface {
 	 */
 	public static function start($argv) {
 		$clInterface = new ClInterface();
-		
-		// remove flyrepo.php from arguments if called php flyrepo.php
-		if($argv[1] == __FILE__) {
-			$argv = array_shift($argv);
-		}
-		
-		$clInterface->setArgv($argv);
+		$clInterface->argv = $argv;
 		return $clInterface;
 	}
 	
@@ -61,21 +65,62 @@ class ClInterface {
 			throw new \Exception('no command given');
 		}
 		
+		// try to find the command and execute it
 		$this->commandName = preg_replace('/[\s\W]+/', '', $this->argv[1]);
-		$commandClass = '\\cabservicesag\\FlyRepo\\Command\\' . ucfirst($this->commandName);
-		$this->show($commandClass);
+		$commandClass = 'cabservicesag\\FlyRepo\\Command\\' . ucfirst($this->commandName);
 		if(!class_exists($commandClass, true)) {
 			throw new \Exception ('command "' . $this->commandName . '" not found.');
 		}
+	
+		$this->options = self::parseOptions($this->availableOptions, $this->argv);
+		$this->setDir();
+		
+		if($commandClass::AUTO_OPEN) {
+			$this->flyRepo = FlyRepo::open($this->dir);
+		}
 		$this->command = new $commandClass($this, $this->argv);
+		$this->command->run();
 		return $this;
 	}
 	
+	public static function parseOptions($availableOptions, $argv) {
+		$options = array();
+		foreach($availableOptions as $option => $description) {
+			$hasvalue = (substr($option, 1, 1) == ':');
+			$mandatory = (substr($option, 1, 2) == '::');
+			$option = substr($option, 0, 1);
+			$argvKey = array_search('-' . $option, $argv);
+			if($argvKey) {
+				if($hasvalue) {
+					$options[$option] = $argv[($argvKey + 1)];
+				} else {
+					$options[$option] = 1;
+				}
+			}
+			if($mandatory && empty($options[$option])) {
+				throw new \Exception('missing argument -' . $option . ' : ' . $description);
+			}
+		}
+		return $options;
+	}
+	
+	public function setDir() {
+		if($this->options['C']) {
+			$this->dir = $this->options['C'];
+		} else {
+			$this->dir = getcwd();
+		}
+	}
+
 	/**
 	 * show usage
 	 */
 	public function showUsage() {
-		$this->show('Usage: flyrepo.php <command>');
+		$optionsExplained = "Options: \n";
+		foreach($this->availableOptions as $option => $description) {
+			$optionsExplained .= "- $option \t " . $description;
+		}
+		$this->show($this->usageInfo . $optionsExplained . "\n");
 	}
 	
 	/**
@@ -85,26 +130,5 @@ class ClInterface {
 	 */
 	public function show($output) {
 		print($output."\n");
-	}
-	
-	
-	/**
-	 * open the fly repository
-	 * 
-	 * @param string $basePath
-	 */
-	public function openFlyRepo($basePath) {
-		$this->flyRepo = FlyRepo::open($basePath);
-	}
-	
-	/**
-	 * set argv 
-	 * 
-	 * @param array $argv
-	 * @return \cabservicesag\FlyRepo\ClInterface
-	 */
-	public function setArgv($argv) {
-		$this->argv = $argv;
-		return $this;
 	}
 }
